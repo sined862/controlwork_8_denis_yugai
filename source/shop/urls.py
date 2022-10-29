fb0ed718e1ccfd2d354b51c1c0546b738cc375f7
Yugai Denis <yugai_denis@mail.ru>
2022-10-29 11:45:14 +0600

    Создание и настройка проекта

---
+++ source/shop/urls.py
@@ -0,0 +1,6 @@
+﻿from django.urls import path
+from shop.views.base import index_view
+
+urlpatterns = [
+    path('', index_view, name='index')
+]
---
+++ source/shop/views/base.py
@@ -0,0 +1,5 @@
+﻿from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'index.html')
